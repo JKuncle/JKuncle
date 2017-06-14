157889bf92b23da997028589b1633069180ac989
JKuncle <xlosove@163.com>
2017-06-14 11:41:53 +0800

    1st commit

---
+++ src/main/java/com/jk/JKuncleApplication.java
@@ -0,0 +1,12 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JKuncleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JKuncleApplication.class, args);
+	}
+}
